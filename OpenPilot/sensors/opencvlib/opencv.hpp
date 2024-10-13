3e26f967bbe7174480c50e18c686f7fd3e9f285c
Sanidhyafeaturist <141141037+Sanidhyafeaturist@users.noreply.github.com>
2024-10-13 20:30:39 +0530

    Update opencv.hpp

--- OpenPilot/sensors/opencvlib/opencv.hpp
+++ OpenPilot/sensors/opencvlib/opencv.hpp
@@ -45,7 +45,7 @@
 
 // File that defines what modules where included during the build of OpenCV
 // These are purely the defines of the correct HAVE_OPENCV_modulename values
-#include "opencv2/opencv_modules.hpp"
+
 
 // Then the list of defines is checked to include the correct headers
 // Core library is always included --> without no OpenCV functionality available
