3e86c1e865327df5e62c1edbe7204d2d5ce662e9
Sanidhyafeaturist <141141037+Sanidhyafeaturist@users.noreply.github.com>
2024-10-13 14:23:26 +0530

    Update camera sensor.cpp

--- OpenPilot/sensors/camera sensor.cpp
+++ OpenPilot/sensors/camera sensor.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include "opencv/include/opencv2/opencv.hpp" // Make sure to link OpenCV in your project
+#include "opencvlib/include/opencv2/opencv.hpp" // Make sure to link OpenCV in your project
 
 class CameraSensor {
 public:
